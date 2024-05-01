83debe21e3f053917d7609131f1d62975f3ecbca
ugochukwuSylva <sylvaspecial@gmail.com>
2024-05-01 22:35:50 +0100

    configuration in next.config.mjs to accommodate enviromental variable

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  serverRuntimeConfig: {
+    // Will only be available on the server side
+    mySecret: "secret",
+    secondSecret: process.env.SECOND_SECRET, // Pass through env variables
+  },
+  publicRuntimeConfig: {
+    // Will be available on both server and client
+    staticFolder: "/static",
+  },
+};
 
 export default nextConfig;
