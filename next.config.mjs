067fc98186479141750bb174209fdc404640764b
ugochukwuSylva <sylvaspecial@gmail.com>
2024-05-01 22:51:33 +0100

    changes in env.local

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,7 @@ const nextConfig = {
   serverRuntimeConfig: {
     // Will only be available on the server side
     mySecret: "secret",
-    secondSecret: process.env.SECOND_SECRET, // Pass through env variables
+    secondSecret: process.env.NEXT_PUBLIC_API_KEY, // Pass through env variables
   },
   publicRuntimeConfig: {
     // Will be available on both server and client
